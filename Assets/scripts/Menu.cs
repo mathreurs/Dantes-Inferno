a0fb72946e11d1388da1ea0ed3bb9ad79a755c8f
mathreur <mathimor127@hotmail.com>
2023-07-05 11:05:59 -0300

    botão start funcionando

---
+++ Assets/scripts/Menu.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour
+{
+    public string nomeCena;
+    public GameObject menuPanel;
+    // Start is called before the first frame update
+    void Start()
+    {
+       //ativando painel do menu
+       menuPanel.SetActive(true);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    //função do botão start
+    public void StartGame()
+    {
+        //carregando a cena game
+        SceneManager.LoadScene(nomeCena);
+    }
+}
