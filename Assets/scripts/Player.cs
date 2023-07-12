1ab2b7604341611fea69ebc95d24a3a03b3078b5
mathreur <mathimor127@hotmail.com>
2023-07-12 10:16:32 -0300

    adicionando colisão

--- Assets/scripts/Player.cs
+++ Assets/scripts/Player.cs
@@ -2,12 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Player : MonoBehaviour
+public class Player : MonoBehaviour 
 {
+    //variavel para rig
+    private Rigidbody2D rig;
     // Start is called before the first frame update
     void Start()
     {
-        
+        rig = GetComponent<Rigidbody2D>(); 
     }
 
     // Update is called once per frame
