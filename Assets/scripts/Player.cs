46d74ab035a04ece8c853c4f13ec36ebc91e7ab6
mathreur <mathimor127@hotmail.com>
2023-08-02 08:42:33 -0300

    morra marta

--- Assets/scripts/Player.cs
+++ Assets/scripts/Player.cs
@@ -6,10 +6,16 @@ public class Player : MonoBehaviour
 {
     //variavel para rig
     private Rigidbody2D rig;
+    //vetor de posição inicial
+    private Vector3 posInicial;
+    //variavel da velocidade
+    private float speed;
     // Start is called before the first frame update
     void Start()
     {
-        rig = GetComponent<Rigidbody2D>(); 
+        rig = GetComponent<Rigidbody2D>();
+        posInicial = new Vector3(0, 0, 0);
+        transform.position = posInicial;
     }
 
     // Update is called once per frame
